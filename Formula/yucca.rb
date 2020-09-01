3b662c482fb3f50878b6599457fb4b8a8e0de5da
l.aminov <l.aminov@tinkoff.ru>
2020-09-01 11:42:05 +0500

    Adds depends_on entries

--- Formula/yucca.rb
+++ Formula/yucca.rb
@@ -3,6 +3,9 @@ class Yucca < Formula
   homepage "http://yucca.app/"
   version "0.3.0"
 
+  depends_on "ffmpeg"
+  depends_on "docker"
+
   if OS.mac?
     url "https://get.yucca.app/v0-3-0/darwin_amd64/yucca"
     sha256 "48185d4fdbc6079e855f881a0e3544431ea28debe4e1547e93d9a0e1f95cd456"
