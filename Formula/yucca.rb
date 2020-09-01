c8782d771c2411ba4c7f3345ce7c16c340339566
l.aminov <l.aminov@tinkoff.ru>
2020-09-01 11:47:45 +0500

    Adds recommended flags

--- Formula/yucca.rb
+++ Formula/yucca.rb
@@ -3,8 +3,8 @@ class Yucca < Formula
   homepage "http://yucca.app/"
   version "0.3.0"
 
-  depends_on "ffmpeg"
-  depends_on "docker"
+  depends_on "ffmpeg" => :recommended
+  depends_on "docker" => :recommended
 
   if OS.mac?
     url "https://get.yucca.app/v0-3-0/darwin_amd64/yucca"
